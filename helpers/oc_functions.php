bd56f04168052ed68d653cc566ea61320ae16542
Fabian Marz <marz.fabian@gmail.com>
2015-11-16 11:13:58 +0100

    Fixed PHP notice for empty image.

--- helpers/oc_functions.php
+++ helpers/oc_functions.php
@@ -662,5 +662,5 @@ function oc_add_esitags($filename, $shortcode, $content = null, $is_callback = f
 function pippin_get_image_id($image_url) {
     global $wpdb;
     $attachment = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE guid='%s';", $image_url ));
-    return $attachment[0];
+    return isset($attachment[0]) ? $attachment[0] : FALSE;
 }
