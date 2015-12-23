0887277e405b5b516021b2e788813acbe19ac6cf
Daniel Kudwien <daniel@netzstrategen.com>
2015-12-23 16:45:39 +0100

    Fixed missing (and wrong default) Cache-Control + Expires headers for ESI endpoint responses.
    mod_expires in .htaccess is configured to use a TTL of 1h for text/html by default, which also applies to ESI responses.
    
    Setting a Cache-Control header is not sufficient; mod_expires only stops interfering with the response if there is a custom Expires header.

--- views/esi/overview.php
+++ views/esi/overview.php
@@ -1,2 +1,7 @@
 <?php
+
+$ttl = 12 * 60 * 60;
+header('Expires: ' . date('r', time() + $ttl));
+header('Cache-Control: max-age=' . $ttl);
+
 require_once dirname(__FILE__) . '/esi_function.php';
--- views/esi/sign.php
+++ views/esi/sign.php
@@ -1,2 +1,7 @@
 <?php
+
+$ttl = 5 * 60;
+header('Expires: ' . date('r', time() + $ttl));
+header('Cache-Control: max-age=' . $ttl);
+
 require_once dirname(__FILE__) . '/esi_function.php';
